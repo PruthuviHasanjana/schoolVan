90090ffade3926910740fad03e4d68d675b02fdc
PruthuviHasanjana <2017e009@eng.jfn.ac.lk>
2021-01-20 09:03:31 +0530

    child model was added

---
+++ lib/models/child.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Child {
+  final String id;
+  final String name;
+  final String school;
+  final int grade;
+
+  Child({
+    @required this.id,
+    @required this.name,
+    @required this.school,
+    @required this.grade,
+  });
+}
