60c80fc2ae34e38cbb04ce1f9cd4cdb830eeb0c4
PruthuviHasanjana <2017e009@eng.jfn.ac.lk>
2021-01-20 08:56:25 +0530

    parent model was updated

--- lib/models/parent.dart
+++ lib/models/parent.dart
@@ -1,17 +1,19 @@
+import 'package:flutter/foundation.dart';
+
 class Parent {
   final String id;
   final String name;
   final String address;
   final String phoneNo;
-  final String balance;
+  double balance;
   final int noOfChildren;
 
-  Parent(
-    this.id,
-    this.name,
-    this.address,
-    this.phoneNo,
-    this.balance,
-    this.noOfChildren,
-  );
+  Parent({
+    @required this.id,
+    @required this.name,
+    @required this.address,
+    @required this.phoneNo,
+    this.balance = 0.0,
+    @required this.noOfChildren,
+  });
 }
